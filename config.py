618229145917ff878da9ce6deba520289923ab50
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-07-18 22:04:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
 BOT_TOKEN = "7560656881:AAFN-uSUCrd2bTnveS-cBPW_anG4Km1c38I"  # Replace with your bot token
-CHANNEL_OR_USER_ID = -1001234567890    # Replace with your channel, group, or user ID
+CHANNEL_OR_USER_ID = -1002310401539    # Replace with your channel, group, or user ID
 FINNHUB_API_KEY = "d1q6pipr01qrh89og8e0d1q6pipr01qrh89og8eg"  # Your real Finnhub API key
 SYMBOL = "OANDA:EUR_USD"  # Example Forex pair
